cadf6ecde0c41bbabcbc93d75af723b8431c32be
Caio Rosa <>
2023-06-11 23:36:31 -0300

    allow listing projects

--- src/cmd/template/list.rs
+++ src/cmd/template/list.rs
@@ -7,20 +7,28 @@ use crate::{cmd::CommandHandler, template::Template};
 use super::ProjectSelector;
 
 #[derive(Parser)]
-#[command(about = "List request templates")]
-pub struct ListCommandHandler;
+#[command(about = "List request templates and projects")]
+pub struct ListCommandHandler {
+    #[arg(long = "projects", help = "List projects")]
+    list_projects: bool,
+}
 
 impl ProjectSelector for ListCommandHandler {}
 
 #[async_trait]
 impl CommandHandler for ListCommandHandler {
     async fn handle(&self) -> Result<()> {
+        if self.list_projects {
+            let projects = Template::list_projects()?;
+            projects.iter().for_each(|project| println!("{project}"));
+
+            return Ok(());
+        }
+
         let project_name = Self::select_project_name(false)?;
         let templates = Template::list(&project_name)?;
 
-        for template in templates {
-            println!("{template}")
-        }
+        templates.iter().for_each(|template| println!("{template}"));
 
         Ok(())
     }
