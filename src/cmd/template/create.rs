e511642daa49d2ff961249fd7ba4cf577373cb56
Caio Rosa <>
2023-11-01 10:39:21 -0300

    change template create command to allow inline arguments instead of wizard

--- src/cmd/template/create.rs
+++ src/cmd/template/create.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 use clap::Parser;
 
@@ -10,21 +10,49 @@ use crate::{
 #[derive(Parser)]
 #[command(about = "Create a request template")]
 pub struct CreateCommandHandler {
-    project: String,
-    template: String,
+    project: Option<String>,
+    template: Option<String>,
+
+    #[arg(long = "variable", help = "Variable name to create")]
+    variable: Option<String>,
+
+    #[arg(long = "project", help = "Project name to create")]
+    new_project: Option<String>,
 }
 
 #[async_trait]
 impl CommandHandler for CreateCommandHandler {
     async fn handle(&self) -> Result<()> {
-        let project = Project::get(&self.project)?;
-        let template = Template::create(project, &self.template)?;
+        if let Some(new_project) = &self.new_project {
+            let project = Project::create(new_project.to_string())?;
 
-        println!(
+            return Ok(println!("Project {} created", project.name));
+        }
+
+        let project_name = self.project.as_ref().ok_or(anyhow!(
+            "Project name is required for variable and template creation"
+        ))?;
+
+        let mut project = Project::get(project_name)?;
+
+        if let Some(variable) = &self.variable {
+            project.create_variable(variable)?;
+
+            return Ok(println!(
+                "Variable {} created for project {}",
+                variable, project.name
+            ));
+        }
+
+        let template_name = self
+            .template
+            .as_ref()
+            .ok_or(anyhow!("Template name is required for creation"))?;
+        let template = Template::create(project, template_name)?;
+
+        Ok(println!(
             "Template {} for project {} saved successfully",
             template.name, template.project.name
-        );
-
-        Ok(())
+        ))
     }
 }
